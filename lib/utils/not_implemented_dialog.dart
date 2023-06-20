fe0b2542dd345d982e77425c8bf50b278c4381da
rjdaniels <danielsrj@protonmail.com>
2023-06-20 16:44:03 +0100

    log 'NotImplementedShown' to GA

--- lib/utils/not_implemented_dialog.dart
+++ lib/utils/not_implemented_dialog.dart
@@ -1,11 +1,15 @@
-
 import 'package:flutter/material.dart';
 
+import 'package:dhali/analytics/analytics.dart';
+
 void showNotImplentedWidget({required BuildContext context, String? feature}) {
-  // TODO : Add logging of feature to Google Analytics
   showDialog(
       context: context,
       builder: (BuildContext _) {
+        gtag(
+            command: "event",
+            target: "NotImplementedShown",
+            parameters: {"feature": feature});
         return const AlertDialog(
           title: Text("This feature has not been implemented yet"),
           content: Text("This is not your fault!"),
