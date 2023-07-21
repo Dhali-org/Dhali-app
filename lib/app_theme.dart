c591c5877234f9b40f156336d1c1323a2e3a50a2
David Simmons <dr.desimmons@gmail.com>
2023-07-21 13:17:38 +0100

    Make drawer items shaded when selected

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -8,6 +8,7 @@ class AppTheme {
   static const Color white = Color(0xFFFFFFFF);
   static const Color nearlyBlack = Color(0xFF213333);
   static const Color grey = Color(0xFF3A5160);
+  static const Color light_purple = Color.fromARGB(125, 142, 142, 246);
   static const Color dark_grey = Color(0xFF313A44);
   static const Color dhali_blue = Color(0xFF0000FF);
   static const Color dhali_blue_highlight = Color.fromARGB(255, 120, 120, 255);
